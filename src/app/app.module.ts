f220ebd8e78ac9e9f83eb5e68a3276af8a5cd06d
Allyson Athyrson <athyrsonallyson@gmail.com>
2020-10-25 12:57:20 -0300

    allyson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { MatIconModule } from '@angular/material';
+import { CarroselComponent } from './pages/carrosel/carrosel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CarroselComponent
   ],
   imports: [
     BrowserModule,
